19fd6beb1392ef8d0eb64ff2616572923f83ae0c
adhyankumar740-cloud <adhyankumar740@gmail.com>
2026-07-26 00:39:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 echo "🔑 Authorizing Telegram Bot..."
 
 while true; do
-    output=$(python3 -m BROKENXMUSIC 2>&1 | tee /tmp/last_run.log)
+    output=$(python3 -u -m BROKENXMUSIC 2>&1 | tee /tmp/last_run.log)
     wait_time=$(echo "$output" | grep -oP "A wait of \K[0-9]+(?= seconds)")
     if [ -n "$wait_time" ]; then
         echo "⏳ FloodWait detected, sleeping for ${wait_time}s..."
