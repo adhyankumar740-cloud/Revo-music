9ee05617cf0adbcc8d7d3494f4e9528a9a63dcdb
adhyankumar740-cloud <adhyankumar740@gmail.com>
2026-07-25 18:27:05 +0530

    Integrate web server start and stop in bot lifecycle
    Added functionality to start and stop the web server during bot initialization and shutdown.

--- BROKENXMUSIC/__main__.py
+++ BROKENXMUSIC/__main__.py
@@ -10,6 +10,7 @@ from BROKENXMUSIC.core.call import Broken
 from BROKENXMUSIC.misc import sudo
 from BROKENXMUSIC.plugins import ALL_MODULES
 from BROKENXMUSIC.utils.database import get_banned_users, get_gbanned
+from BROKENXMUSIC.utils.webserver import start_web_server, stop_web_server
 from config import BANNED_USERS
 
 async def init():
@@ -37,6 +38,7 @@ async def init():
         importlib.import_module("BROKENXMUSIC.plugins" + all_module)
     LOGGER("BROKENXMUSIC.plugins").info("Successfully Imported Modules...")
     await userbot.start()
+    await start_web_server()
     await Broken.start()
     try:
         await Broken.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
@@ -52,6 +54,7 @@ async def init():
         "\x42\x72\x6f\x6b\x65\x6e\x20\x78\x20\x53\x70\x6f\x74\x69\x66\x79\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x20\x0a\x0a\x44\x6f\x6e\x27\x74\x20\x46\x6f\x72\x67\x65\x74\x20\x54\x6f\x20\x56\x69\x73\x69\x74\x20\x40\x42\x72\x6f\x6b\x65\x6e\x78\x4e\x65\x74\x77\x6f\x72\x6b"
     )
     await idle()
+    await stop_web_server()
     await app.stop()
     await userbot.stop()
     LOGGER("BROKENXMUSIC").info("Stopping Brokenxnetwork Music Bot...")
