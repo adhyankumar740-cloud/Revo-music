b2d5d4456bd75bb2da670587b441fc198bfbe5ee
adhyankumar740-cloud <adhyankumar740@gmail.com>
2026-07-25 11:28:49 +0530

    Update __init__.py

--- BROKENXMUSIC/platforms/__init__.py
+++ BROKENXMUSIC/platforms/__init__.py
@@ -2,6 +2,7 @@ from .Apple import AppleAPI
 from .Carbon import CarbonAPI
 from .Resso import RessoAPI
 from .Soundcloud import SoundAPI
+from .SongCache import SongCacheAPI
 from .Spotify import SpotifyAPI
 from .Telegram import TeleAPI
 from .TgScrap import TgScrapAPI
