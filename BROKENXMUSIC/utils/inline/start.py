918bdcc7f48975032f7ebbcbe99c8c6d626d844c
adhyankumar740-cloud <adhyankumar740@gmail.com>
2026-07-28 09:54:36 +0530

    Clean up start.py by removing unused code
    Removed unused repo import and callback query handler.

--- BROKENXMUSIC/utils/inline/start.py
+++ BROKENXMUSIC/utils/inline/start.py
@@ -1,6 +1,5 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton
-from BROKENXMUSIC.plugins.bot.repo import start
 import config
 from BROKENXMUSIC import app
 
@@ -26,9 +25,6 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_7"], callback_data="repo_callback"),   
-        ],        
-        [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"), 
         ],
         [
@@ -37,10 +33,4 @@ def private_panel(_):
         ],
      ]
     return buttons
-    
-@app.on_callback_query(filters.regex("repo_callback"))
-async def repo_callback(client, callback_query):
-    message = callback_query.message
-    await start(client, message)  
-    await callback_query.answer()
 
